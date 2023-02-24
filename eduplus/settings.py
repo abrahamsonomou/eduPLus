d239dc49c054b057ac2e75e79451209ff2dc2775
sonomousekou <sonomousekou@gmail.com>
2023-02-24 13:33:13 +0100

    24-02-2023

--- eduplus/settings.py
+++ eduplus/settings.py
@@ -38,6 +38,7 @@ TIERS_APPS=[
     'rosetta',
     'taggit',
     'django_ckeditor_5',
+    'import_export',
 ]
 
 PROJECT_APPS=[
@@ -86,23 +87,23 @@ WSGI_APPLICATION = 'eduplus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_eduplus',
-        'USER': 'eduplususer',
-        'PASSWORD': 'p@ssw0rd',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_eduplus',
+#         'USER': 'eduplususer',
+#         'PASSWORD': 'p@ssw0rd',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
