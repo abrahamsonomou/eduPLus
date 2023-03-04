ef4d1a2bc0ecb6d02e76965aa6ea7b8ef9605998
abrahamsonomou <sonomouabrahamsekou97@gmail.com>
2023-03-04 22:28:45 +0000

    make it better

--- eduplus/settings.py
+++ eduplus/settings.py
@@ -11,7 +11,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -336,5 +335,4 @@ ADMIN_EMAIL = "contact@sonomou.tech"
 SUPPORT_EMAIL = "contact@sonomou.tech"
 DEFAULT_FROM_EMAIL = ADMIN_EMAIL
 SERVER_EMAIL = ADMIN_EMAIL
-
 
